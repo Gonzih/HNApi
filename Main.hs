c5698336ae26215f352557689997b0103a6fdab9
Max Gonzih <gonzih@gmail.com>
2014-08-12 19:36:32 +0200

    Initialize thread loop

--- Main.hs
+++ Main.hs
@@ -4,11 +4,27 @@ module Main where
 
 import Data.IORef
 import Control.Monad.IO.Class
+import Control.Concurrent (forkIO, threadDelay)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as TE
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Parser
+
+updateRefWithJson :: IORef T.Text -> IO ()
+updateRefWithJson cache = do
+    jsonBS <- fetchAndParse
+    let encodedJsonData = TE.decodeUtf8 jsonBS
+    _ <- atomicModifyIORef' cache (encodedJsonData,)
+
+    putStrLn "Data was fetched"
+
+updateLoop :: IORef T.Text -> IO ()
+updateLoop ref = do
+    updateRefWithJson ref
+    threadDelay 300000000
+    updateLoop ref
+    return ()
 
 main :: IO ()
 main = scotty 4000 $ do
@@ -16,14 +32,7 @@ main = scotty 4000 $ do
 
     cache <- liftIO $ newIORef T.empty
 
-    post "/" $ do
-        jsonBS <- liftIO $ fetchAndParse
-        let encodedJsonData = TE.decodeUtf8 jsonBS
-
-        _ <- liftIO $ putStrLn "Data was fetched"
-        _ <- liftIO $ atomicModifyIORef' cache (encodedJsonData,)
-        text encodedJsonData
-        setHeader "content-type" "application/json"
+    _ <- liftIO $ forkIO $ updateLoop cache
 
     get "/" $ do
         jsonText <- liftIO $ readIORef cache
